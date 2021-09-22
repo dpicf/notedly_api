b6b6838237efe38569b84ce14a2ebca1da1669d7
Dmitry <picfay@ya.ru>
2021-09-22 12:22:45 +0300

    first app index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+const express = require('express'); // устанавливаем зависимость в константу
 
+const app = express(); // создаём объект app
+const port = process.env.PORT || 4000; // установить порт 4000
+
+app.get('/', (req, res) => res.send('Hello World')); // при обращении к корневому каталогу выводить Hello World!!!
+
+app.listen(port, () =>
+  console.log(`Сервер запущен http://localhost:${port}`) // выводить в консоль сообщение при помощи callback-функции
+);
